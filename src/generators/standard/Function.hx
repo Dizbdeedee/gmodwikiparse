869be66877cff7413d1792c97518fe0ab2c5247c
Dizbdeedee <dizbeegit@mentice.cotse.net>
2022-11-30 21:46:59 +0000

    Uploading
    FossilOrigin-Name: 5703ca434c66045e0033e781428db5cbf3dd13723c62203187b248f12b7b0c4d
    
    FossilOrigin-Name: 4a497230f9cbdb1db771646c4cc0ef589be9167633d0b373b7bf6460929dc39f

--- src/generators/standard/Function.hx
+++ src/generators/standard/Function.hx
@@ -9,33 +9,35 @@ interface FunctionPublisher {
 
 class FunctionPublisherDef {
     final dbConnection:WikiDB;
-    
-    public function new(_dbConnection:WikiDB) {
+
+    final functionParser:FunctionParser;
+
+    public function new(_dbConnection:WikiDB,_functionParser:FunctionParser) {
         dbConnection = _dbConnection;
     }
 
     @:async public function publishFunction(url:String,jq:CheerioAPI) {
-		var create = @:await parseFunction(url,jq);
-		var funcID = @:await dbConnection.Function.insertOne(create.func);
-		var mappedFuncArgs = create.funcargs.map((funcarg) -> {
-			var copy = Reflect.copy(funcarg);
-			untyped copy.funcid = funcID;
-			return copy;
-		});
-		var mappedFuncRets = create.funcrets.map((funcret) -> {
-			var copy = Reflect.copy(funcret);
-			untyped copy.funcid = funcID;
-			return copy;
-		});
-		var mappedLuaExamples = create.luaexamples.map((luaexample) -> {
-			var copy = Reflect.copy(luaexample);
-			untyped copy.funcid = funcID;
-			return copy;
-		});
-		@:await dbConnection.FunctionArg.insertMany(mappedFuncArgs).eager();
-		@:await dbConnection.FunctionRet.insertMany(mappedFuncRets).eager();
-		@:await dbConnection.LuaExample.insertMany(mappedLuaExamples).eager();
-		return Noise;
-	}
+        var create = @:await parseFunction(url,jq);
+        var funcID = @:await dbConnection.Function.insertOne(create.func);
+        var mappedFuncArgs = create.funcargs.map((funcarg) -> {
+            var copy = Reflect.copy(funcarg);
+            untyped copy.funcid = funcID;
+            return copy;
+        });
+        var mappedFuncRets = create.funcrets.map((funcret) -> {
+            var copy = Reflect.copy(funcret);
+            untyped copy.funcid = funcID;
+            return copy;
+        });
+        var mappedLuaExamples = create.luaexamples.map((luaexample) -> {
+            var copy = Reflect.copy(luaexample);
+            untyped copy.funcid = funcID;
+            return copy;
+        });
+        @:await dbConnection.FunctionArg.insertMany(mappedFuncArgs).eager();
+        @:await dbConnection.FunctionRet.insertMany(mappedFuncRets).eager();
+        @:await dbConnection.LuaExample.insertMany(mappedLuaExamples).eager();
+        return Noise;
+    }
 
 }
